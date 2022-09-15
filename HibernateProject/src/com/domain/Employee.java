88b9b3dd1b40dd440dadf268e3408cbb7d07acfe
panchalleena1286 <panchalleena8839@gmail.com>
2022-09-15 19:02:02 +0530

    initial commit

---
+++ HibernateProject/src/com/domain/Employee.java
@@ -0,0 +1,37 @@
+package com.domain;
+
+public class Employee {
+	private int empNo;
+	private String empName;
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(int empNo, String empName) {
+		super();
+		this.empNo = empNo;
+		this.empName = empName;
+	}
+
+	public int getEmpNo() {
+		return empNo;
+	}
+
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+	
+	
+
+}
